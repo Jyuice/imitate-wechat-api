7e82c649824f48172e583ddffe4d88f052e289e9
Jyuice <60843463+Jyuice@users.noreply.github.com>
2022-09-13 07:05:31 +0800

    隐藏数据库信息

--- module/config/mysql_config.js
+++ module/config/mysql_config.js
@@ -1,8 +1,8 @@
 const mysqlConfig = {
-  host     : '106.52.45.38',   // 数据库地址
-  user     : 'justin',    // 数据库用户
-  password : 'woshimima',   // 数据库密码
-  database : 'justin'  // 选中数据库
+  host     : 'localhost',   // 数据库地址
+  user     : 'root',    // 数据库用户
+  password : 'password',   // 数据库密码
+  database : 'databaseName'  // 选中数据库
 }
 
 module.exports = mysqlConfig
